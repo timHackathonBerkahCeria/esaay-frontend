18793c21768cfc27b3dab3b13f396c6758f99046
ray_arwindra <wan.muhammad81@ui.ac.id>
2021-08-08 21:09:19 +0700

    create homepage

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/globals.scss'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+      <>
+        <Head>
+          <link 
+            rel="preconnect" 
+            href="https://fonts.googleapis.com"
+          />
+          <link 
+            rel="preconnect" 
+            href="https://fonts.gstatic.com" 
+            crossorigin
+          />
+          <link 
+            href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&family=Hind+Madurai:wght@400;700&display=swap" 
+            rel="stylesheet"
+          />
+        </Head> 
+        <Component {...pageProps} />
+      </>
+    )
 }
 
 export default MyApp
---
+++ pages/index.jsx
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import styles from '../styles/pages/Home.module.scss'
+import Image from 'next/image'
+import { 
+  Navbar, 
+  Nav,
+  Col,
+  Row
+} from 'react-bootstrap'
+import landingImg from '../public/student.png'
+
+export default function Home() {
+  return (
+    <div className={styles.home}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar bg="light" expand="lg" className={styles.navBar}>
+          <Navbar.Brand className={styles.logo}>exaam</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link className={styles.login} href="#home">Login/Sign Up</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+
+      <main>
+        <Row>
+          <Col className={styles.landingText}>
+            <h1>Welcome to <span>exaam</span></h1>
+            <p>Take and correct exams quickly.</p>
+            <button>Get Started</button>
+          </Col>
+          <Col>
+            <Image 
+              src={landingImg}
+              className={styles.landingImg}
+            />
+          </Col>
+        </Row>
+
+      </main>
+
+
+    </div>
+  )
+}
