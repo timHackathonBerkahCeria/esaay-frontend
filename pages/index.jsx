920cd8ebe2591de6e5dca2901c3000c0f8e7cbbe
ray_arwindra <wan.muhammad81@ui.ac.id>
2021-08-09 09:04:46 +0700

    change title

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <div className={styles.home}>
       <Head>
-        <title>Create Next App</title>
+        <title>Exaam | Take exams and get quick results.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
